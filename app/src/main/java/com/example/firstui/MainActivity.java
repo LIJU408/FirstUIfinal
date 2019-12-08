96a2a0e6b5e31b5e5e59df1f9146c0393b306f77
LIJU408 <liju2@illinois.edu>
2019-12-08 15:16:10 -0600

    cs 125 final project(final version)

--- app/src/main/java/com/example/firstui/MainActivity.java
+++ app/src/main/java/com/example/firstui/MainActivity.java
@@ -8,8 +8,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
+
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -21,62 +23,70 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import org.json.JSONException;
+
 import org.json.JSONObject;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //youtube link https://youtu.be/ixS8fQj_1NY
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.searchButton);
-        final String key = "NYC";
-        System.out.println("at the top");
-        Log.e("top", "at the top");
-        TextView textView = findViewById(R.id.searchWindow);
+        final EditText textView = findViewById(R.id.searchWindow);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String location = textView.getText().toString();
                 RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                final String key = "NYC";
-                String url = "https://api.yelp.com/v3/businesses/search/" + key;
-                JSONObject otherKey = new JSONObject();
-                try {
-                    otherKey.put("Authorization", "m3Sfq0JodxB0SUWWLSwBiG-y_W2fBT7zzgavNLXFsfuBfFKVE6CYX6r6qZoTA_4VVumu4JC1brHIDv9EJouOd4Uy8LQsJw-XoPrmTzaXzFzz66hMR0aRXWGohOjmXXYx");
-                } catch(JSONException e) {
-                    Log.e("exception",e.getMessage());
-
-                }
-
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, otherKey, new Response.Listener<JSONObject>() {
+                //the URL's format; the api requires query parameters
+                String url = "https://api.yelp.com/v3/businesses/search?location=" + location;
+                queue.add(new JsonObjectRequest(Request.Method.GET, url, (JSONObject)null, new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        System.out.println("just begin");
                         String raw = response.toString();
                         JsonParser parser = new JsonParser();
-                        //root
                         JsonObject root = parser.parse(raw).getAsJsonObject();
                         JsonArray business = root.get("businesses").getAsJsonArray();
                         int count = 0;
-                        System.out.println("after getting");
+                        //using JSON to get the information showed in the second activity
                         for (JsonElement a : business) {
-                            Boolean i = a.getAsJsonObject().get("is_closed").getAsBoolean();
-                            if (!i) {
-                                String id = a.getAsJsonObject().get("id").getAsString();
-                                MainActivity2.responseFromApi.add(id);
-                                count++;
-                                if (count == 4) {
-                                    break;
-                                }
+                            String name = a.getAsJsonObject().get("name").getAsString();
+                            String rating = a.getAsJsonObject().get("rating").getAsString();
+                            String phone = a.getAsJsonObject().get("phone").getAsString();
+                            boolean closedOrNot = a.getAsJsonObject().get("is_closed").getAsBoolean();
+                            int reviewCount = a.getAsJsonObject().get("review_count").getAsInt();
+                            String pictureURLList = a.getAsJsonObject().get("image_url").getAsString();
+                            JsonArray categories = a.getAsJsonObject().get("categories").getAsJsonArray();
+                            MainActivity2.nameList.add(name);
+                            MainActivity2.ratingList.add(rating);
+                            MainActivity2.phoneList.add(phone);
+                            MainActivity2.isClosedList.add(closedOrNot);
+                            MainActivity2.reviewCountList.add(reviewCount);
+                            MainActivity2.pictureURLList.add(pictureURLList);
+                            String category = "";
+                            //category
+                            for (JsonElement b:categories) {
+                                JsonObject current = b.getAsJsonObject();
+                                String title = current.get("title").getAsString() + ",";
+                                category = category + title;
+                            }
+                            category = category.substring(0, category.length() - 1);
+                            MainActivity2.titleList.add(category);
+                            count++;
+                            if (count == 100) {
+                                break;
                             }
                         }
-                        System.out.println("after arraylistadding");
-                        //Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                        //startActivity(intent);
-                        System.out.println("have I start the activity?");
-
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
                     }
                 }, new Response.ErrorListener() {
 
@@ -84,12 +94,22 @@ public class MainActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error) {
                         System.out.println(error.getMessage());
                         error.printStackTrace();
-                        Log.e("bottom", "at the bottom");
+                    }
+                }){
+                    @Override
+                    //add authorization to get the api usable
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        try {
+                            Map<String, String> map = new HashMap<String, String>();
+                            map.put("Authorization", "Bearer m3Sfq0JodxB0SUWWLSwBiG-y_W2fBT7zzgavNLXFsfuBfFKVE6CYX6r6qZoTA_4VVumu4JC1brHIDv9EJouOd4Uy8LQsJw-XoPrmTzaXzFzz66hMR0aRXWGohOjmXXYx");
+                            return map;
+                        } catch (Exception e) {
+                            System.out.println("test error");
+                            Log.e("error", "Authentication Filure" );
+                        }
+                        return super.getHeaders();
                     }
                 });
-                queue.add(jsonObjectRequest);
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/firstui/MainActivity2.java
+++ app/src/main/java/com/example/firstui/MainActivity2.java
@@ -1,30 +1,81 @@
 package com.example.firstui;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.squareup.picasso.Picasso;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
-    public static ArrayList<String> responseFromApi = new ArrayList<>();
+    public static ArrayList<String> nameList = new ArrayList<>();
+    public static ArrayList<String> ratingList = new ArrayList<>();
+    public static ArrayList<Boolean> isClosedList = new ArrayList<>();
+    public static ArrayList<String> phoneList = new ArrayList<>();
+    public static ArrayList<Integer> reviewCountList = new ArrayList<>();
+    public static ArrayList<String> pictureURLList = new ArrayList<>();
+    public static ArrayList<String> titleList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        EditText textView = findViewById(R.id.editText);
-        textView.setText("");
-        for (int i = 0; i < responseFromApi.size(); i++) {
-            textView.append(responseFromApi.get(i));
-            textView.append("\n");
-            System.out.println(i);
+        LinearLayout information = findViewById(R.id.information);
+        for (int i = 0; i < nameList.size(); i++) {
+            String current = "name :" + nameList.get(i) + "\n";
+            String current1 = "phone :" + phoneList.get(i) + "\n";
+            String current2 = "rate :" + ratingList.get(i) + "\n";
+            String current3 = "categories :" + titleList.get(i) + "\n";
+            String current4 = pictureURLList.get(i);
+            //SHOW(chunk part)
+            View chunk = getLayoutInflater().inflate(R.layout.chunk_restaurtant, information, false);
+            ImageView picture = chunk.findViewById(R.id.picture);
+            //SHOW(transfer string to url; display images using Picasso library)
+            //Picasso.with(Context).load(Uri).into(imageView)
+            //parse the String into Uri
+            Uri uri = Uri.parse(current4);
+            Picasso.with(this.getApplicationContext()).load(uri).into(picture);
+            picture.setVisibility(View.VISIBLE);
+            TextView name = chunk.findViewById(R.id.name);
+            name.setText(current);
+            //SHOW(if the restaurant is closed, the name's color will be turned into grey)
+            if (isClosedList.get(i)) {
+                name.setTextColor(Color.parseColor("#A89594"));
+            }
+            TextView phone = chunk.findViewById(R.id.phone);
+            phone.setText(current1);
+            TextView rate = chunk.findViewById(R.id.rate);
+            rate.setText(current2);
+            TextView title = chunk.findViewById(R.id.title);
+            title.setText(current3);
+            chunk.setVisibility(View.VISIBLE);
+            information.addView(chunk);
         }
+        //clear all the list for next location
+        nameList.clear();
+        phoneList.clear();
+        ratingList.clear();
+        titleList.clear();
+        pictureURLList.clear();
         Button button = findViewById(R.id.returnButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,4 +86,21 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
     }
+    /*private Bitmap getImageBitmap(String url) {
+        Bitmap bm = null;
+        try {
+            URL aURL = new URL(url);
+            URLConnection conn = aURL.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bm = BitmapFactory.decodeStream(bis);
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+            Log.e("tag", "error");
+        }
+        return bm;
+    }*/
 }
+
